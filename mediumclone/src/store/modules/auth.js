90a5d587dc45f946bb924ff12134d34eee167b16
Pavel Sokolov <frontbastard@gmail.com>
2021-01-21 01:41:35 +0200

    Add token to localStorage

--- mediumclone/src/store/modules/auth.js
+++ mediumclone/src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import authApi from '@/api/auth'
+import {setItem} from '@/helpers/persistanceStorage'
 
 const state = {
   isSubmitting: false,
@@ -31,6 +32,7 @@ const actions = {
         .then((response) => {
           console.log(response)
           context.commit('registerSuccess', response)
+          setItem('accessToken', response.data.user.token)
           resolve(response.data.user)
         })
         .catch((result) => {
